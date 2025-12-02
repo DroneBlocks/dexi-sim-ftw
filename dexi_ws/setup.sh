b217b053d9d65d74a67ae9e7329cea2b905c808a
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-12-01 22:22:23 -0600

    Fix AprilTag dependencies: add camera-info-manager, remove camera_ros from build

--- dexi_ws/setup.sh
+++ dexi_ws/setup.sh
@@ -21,8 +21,8 @@ source /opt/ros/humble/setup.bash
 # Build only DEXI packages and required dependencies
 echo "Building DEXI packages: px4_msgs, dexi_interfaces, dexi_offboard, dexi_led, dexi_bringup, apriltag packages..."
 colcon build --packages-select px4_msgs dexi_interfaces dexi_offboard dexi_led dexi_bringup \
-    apriltag apriltag_msgs apriltag_ros camera_ros dexi_apriltag \
-  --packages-ignore dexi_cpp dexi_camera dexi_yolo \
+    apriltag apriltag_msgs apriltag_ros dexi_apriltag \
+  --packages-ignore dexi_cpp dexi_camera dexi_yolo camera_ros \
     compressed_depth_image_transport compressed_image_transport \
     theora_image_transport zstd_image_transport image_transport_plugins cv_bridge \
   --symlink-install
